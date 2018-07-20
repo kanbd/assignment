36e1aec7bca85709810f3d40bfdb362e02863709
kanbd <kanbdsh@gmail.com>
2018-07-19 22:03:12 -0400

    good

--- a1/a1.js
+++ a1/a1.js
@@ -9,23 +9,29 @@ function do_shift(){
 		if(left.length!=0){
 				document.getElementById('left_box').value=left.slice(1);
 				document.getElementById('right_box').value=right+left.slice(0,1);
+
 		}	
 	}else if(way=='left'&&state){
 		var left=document.getElementById('left_box').value;
 		var right=document.getElementById('right_box').value;
-		if(right.length!=0{
+		if(right.length!=0){
 			document.getElementById('right_box').value=right.slice(0,right.length-1);
 			document.getElementById('left_box').value=right.slice(right.length-1)+left;
+			//setTimeout(do_shift(),1000);
 		}
 	}
-	setTimeout(do_shift(),1000);
+	//console.log("hello")
+	setTimeout(do_shift,1000);
 }
 function do_dir(ways){
+	state=true;
 	way=ways;
 }
 
 function stop(){
 
-	state=~state;
+	state=!state;
+	console.log(state);
 	
 }
+do_shift();
