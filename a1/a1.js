39352f262fe7e85bdfdc6afb73745d2b427a687e
kanbd <kanbdsh@gmail.com>
2018-07-19 20:22:55 -0400

    Update a1.js

--- a1/a1.js
+++ a1/a1.js
@@ -1,49 +1,31 @@
-var running_l=true;
-var running_r=false;
+var state=ture;
 
-function right_shift(){
+function do_shift(way){
 	
-	running_r=true;
-	running_l=false;
-
-	function do_right(){
+	if(way=='right'&&state){
+		state=way;
 		var left=document.getElementById('left_box').value;
 		var right=document.getElementById('right_box').value;
 
-		if(left.length!=0&&running_r){
-			setTimeout(function shift(){
+		if(left.length!=0){
 				document.getElementById('left_box').value=left.slice(1);
 				document.getElementById('right_box').value=right+left.slice(0,1);
-				do_right();
-			},1000);
 		}	
-	}
-	do_right();
-}
-
-function left_shift(){
-	
-	running_l=true;
-	running_r=false;
-
-	function do_left(){
+	}else if(way=='left'&&state){
+		state=way;
 		var left=document.getElementById('left_box').value;
 		var right=document.getElementById('right_box').value;
-
-		if(right.length!=0&&running_l){
-			setTimeout(function shift(){
-				document.getElementById('right_box').value=right.slice(0,right.length-1);
-				document.getElementById('left_box').value=right.slice(right.length-1)+left;
-				do_left();
-			},1000);
+		if(right.length!=0{
+			document.getElementById('right_box').value=right.slice(0,right.length-1);
+			document.getElementById('left_box').value=right.slice(right.length-1)+left;
 		}
 	}
-	do_left();
+	setTimeout(do_shift(),1000);
 }
+
 
 function stop(){
 
-	running_l=false;
-	running_r=false;
+	state=~state;
 	
 }
